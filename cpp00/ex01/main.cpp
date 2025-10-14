757cfe1434f0924f54c9fc88ccdad8b9810e0bff
a-hakki <abdelazizhatake@gmail.com>
2025-10-14 16:43:12 +0100

    working on cpp/cpp00/ex01 | progress 10%

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+#include "main.hpp"
+
+int main()
+{
+    PhoneBook	phonebook;
+	str			line;
+	str			ADD = "ADD";
+	str			SEARCH = "SEARCH";
+	str			EXIT = "EXIT";
+
+	std::cout << "Hey..., Well this is your contact manager!" << std::endl;
+    while (1)
+    {
+        std::cin >> 
+    }
+}
---
+++ cpp00/ex01/main.hpp
@@ -0,0 +1,14 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+typedef std::string str;
+
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+#endif
