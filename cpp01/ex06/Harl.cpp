e6a3702169e9c79f742008e625b0f82b171c75db
a-hakki <abdelazizhatake@gmail.com>
2025-11-04 09:04:02 +0100

    cpp01/ex06 done

---
+++ cpp01/ex06/Harl.cpp
@@ -0,0 +1,76 @@
+#include "Harl.hpp"
+
+void Harl::debug(void)
+{
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void)
+{
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void)
+{
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void)
+{
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void Harl::complain(std::string level)
+{
+    void (Harl::*funcs[])(void) = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+
+    const std::string levels[] = {
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR"
+    };
+
+    int idx = -1;
+    for (size_t i = 0; i < 4; ++i)
+    {
+        if (level == levels[i])
+        {
+            idx = static_cast<int>(i);
+            break;
+        }
+    }
+
+    switch (idx)
+    {
+        case 0:
+            (this->*funcs[0])();
+        case 1:
+            (this->*funcs[1])();
+        case 2:
+            (this->*funcs[2])();
+        case 3:
+            (this->*funcs[3])();
+            break;        
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+            break;
+    }
+}
