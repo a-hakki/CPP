2f866fc5ca1c4604bbca6c3a50f64fc1cfa19d95
a-hakki <abdelazizhatake@gmail.com>
2026-07-16 12:14:53 +0100

    done

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -4,12 +4,13 @@ int main()
 {
     // Test with a vector of integers
     {   
-        std::vector<int> vec;
+        std::list<int> vec;
         for (int i = 0; i < 10; i++)
             vec.push_back(i);
         try
         {
             std::cout << easyfind(&vec, 5) << std::endl;
+            std::cout << easyfind(&vec, 10) << std::endl;
         }
         catch (std::exception &e)
         {
